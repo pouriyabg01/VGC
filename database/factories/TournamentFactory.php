8e2ba8ea8cae272a88b41fcb7cb346b73258bbcf
Pouriya <amiralibg0135@gmail.com>
2026-07-01 03:40:05 +0330

    add factory and seeder user , plan,subs,tournament,tournament_useers

---
+++ database/factories/TournamentFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Enums\Tournaments\TournamentEnum;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Tournament>
+ */
+class TournamentFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'game' => fake()->randomElement(['fifa' , 'nfs' , 'mortal' , 'crash' , 'GT7' , 'PES']),
+            'end_at' => null,
+            'winner_id' => null,
+//            'status' => TournamentEnum::PENDING //default value is pending
+        ];
+    }
+}
